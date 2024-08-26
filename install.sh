7b6225b9ddd289399977475374b86d55b6469b47
David Chu <davidchu@Davids-MacBook-Pro.local>
2024-08-26 18:03:07 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install fzf bat ripgrep the_silver_searcher perl universal-ctags tmux vim git
+brew install fzf bat ripgrep the_silver_searcher perl universal-ctags tmux vim git koekeishiya/formulae/yabai koekeishiya/formulae/skhd
 sudo npm install -g lessc swagger-ui-watcher
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -13,11 +13,15 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # Install Color-theme
 git clone https://github.com/lifepillar/vim-solarized8.git ~/.vim/pack/themes/opt/solarized8
 
-ln -s ./.vimrc ~/.vimrc
+ln -s `pwd`/.vimrc ~/.vimrc
 
-ln -s ./coc-settings.json ~/.vim/coc-settings.json
+ln -s `pwd`/coc-settings.json ~/.vim/coc-settings.json
 
-ln -s ./.tmux.conf ~/.tmux.conf
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
+
+ln -s `pwd`/yabai/yabairc ~/.yabairc
+
+ln -s `pwd`/skhd/skhdrc ~/.skhdrc
 
 tmux source ~/.tmux.conf
 
