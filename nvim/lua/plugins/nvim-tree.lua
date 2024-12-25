fefc66171fa65e20f782e514805944337c8fde98
David <davidaasm@gmail.com>
2024-12-25 13:26:50 +0800

    update

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,19 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    local function my_on_attach(bufnr)
+      local api = require("nvim-tree.api")
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
+      -- open file in new tab
+      vim.keymap.set("n", "t", api.node.open.tab, opts("Open file in new tab"))
+    end
+
+    require("nvim-tree").setup({
+      on_attach = my_on_attach
+    })
   end,
 }
