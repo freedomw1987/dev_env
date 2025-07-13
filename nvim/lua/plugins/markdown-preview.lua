e6ab1b27ee2e13ab6c580cda477b8f97da1eae81
David @ Home <david@marsgroup.ltd>
2025-07-14 00:15:02 +0800

    update

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,8 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    ft = { "markdown" },
+  "iamcco/markdown-preview.nvim",
+
+  build = "cd app && yarn install",
+
+  enable = false
+
 }
