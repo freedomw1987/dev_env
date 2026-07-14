26ca023d55db94b78afed7ae655f39862602f3bb
apple <apple@appledeMac-mini.local>
2026-07-14 23:15:40 +0800

    fixed: blink bug

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -7,5 +7,21 @@ return {
   version = '*', -- 或者你原本指定的版本/分支
   opts = {
     -- 你原本的 blink.cmp 設定 ...
-  }
+  },
+  config = function()
+    require('blink.cmp').setup({
+      sources = {
+        -- 1. 確保 default 列表中有 'lazydev'
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100, -- 讓 lazydev 的補全順序排在最前面
+          },
+        },
+      },
+    })
+  end
 }
